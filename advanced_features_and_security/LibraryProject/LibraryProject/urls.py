630c629e2d6bb21ed915a3f9cc7bb5afa0e1e5c2
ilham mouatakif <mouatakif3ilham@gmail.com>
2025-12-07 20:33:57 +0100

    C1

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -22,3 +22,9 @@ urlpatterns = [
     path('', include('bookshelf.urls')), # Assuming existing config
     path('relationship/', include('relationship_app.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
