1916b070d8880ca3a8ea33820d21bafab7713f33
ilham mouatakif <mouatakif3ilham@gmail.com>
2025-12-21 23:13:45 +0100

    Q5

--- social_media_api/posts/tests.py
+++ social_media_api/posts/tests.py
@@ -1,3 +1,69 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from .models import Post, Comment
 
-# Create your tests here.
+User = get_user_model()
+
+class PostTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.force_authenticate(user=self.user)
+        self.post_data = {'title': 'Test Post', 'content': 'Test Content'}
+        self.post = Post.objects.create(author=self.user, title='Existing Post', content='Content')
+
+    def test_create_post(self):
+        response = self.client.post(reverse('post-list'), self.post_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 2)
+        self.assertEqual(Post.objects.get(id=response.data['id']).author, self.user)
+
+    def test_update_post(self):
+        url = reverse('post-detail', args=[self.post.id])
+        updated_data = {'title': 'Updated Title', 'content': 'Updated Content'}
+        response = self.client.put(url, updated_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.post.refresh_from_db()
+        self.assertEqual(self.post.title, 'Updated Title')
+
+    def test_delete_post(self):
+        url = reverse('post-detail', args=[self.post.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Post.objects.count(), 0)
+
+    def test_filter_posts(self):
+        Post.objects.create(author=self.user, title='Another Post', content='Searchable Content')
+        response = self.client.get(reverse('post-list') + '?search=Searchable')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Another Post')
+
+class CommentTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.force_authenticate(user=self.user)
+        self.post = Post.objects.create(author=self.user, title='Test Post', content='Content')
+        self.comment_data = {'post': self.post.id, 'content': 'Test Comment'}
+        self.comment = Comment.objects.create(post=self.post, author=self.user, content='Existing Comment')
+
+    def test_create_comment(self):
+        response = self.client.post(reverse('comment-list'), self.comment_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Comment.objects.count(), 2)
+        self.assertEqual(Comment.objects.get(id=response.data['id']).author, self.user)
+
+    def test_update_comment(self):
+        url = reverse('comment-detail', args=[self.comment.id])
+        updated_data = {'post': self.post.id, 'content': 'Updated Comment'}
+        response = self.client.put(url, updated_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.comment.refresh_from_db()
+        self.assertEqual(self.comment.content, 'Updated Comment')
+
+    def test_delete_comment(self):
+        url = reverse('comment-detail', args=[self.comment.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Comment.objects.count(), 0)
